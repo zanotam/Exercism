89403b60c84d3993ccf7a3bb34fbee4801f89303
Robert Baker <zanotam@gmail.com>
2020-02-05 17:30:44 -0700

    Improve readability of the Luhn function and fixed an error-error.

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -10,7 +10,7 @@ import (
 
 //Valid Checks if a string passes the Luhn test
 func Valid(digits string) (isValid bool) {
-	strippedDigits := strings.Replace(digits, " ", "", -1)
+	strippedDigits := strings.ReplaceAll(digits, " ", "")
 	err, summedDigits := luhnDoubleAndSum(strippedDigits)
 	if err == nil {
 		isValid = (summedDigits%10) == 0 && (len(strippedDigits) > 1)
@@ -22,25 +22,24 @@ func Valid(digits string) (isValid bool) {
 //switched to error first because I like that convention
 func luhnDoubleAndSum(digits string) (err error, summed int) {
 	//var doubled string;
-	for i, digit := range []rune(digits) {
+	for i, digit := range digits {
 		//convert character into a number
-		var array = make([]rune, 1)
-		array[0] = digit
-		str := string(array)
-		number, numberCheck := strconv.Atoi(str)
+		var sliceDigit = make([]rune, 1)
+		sliceDigit[0] = digit
+		strDigit := string(sliceDigit)
+		numDigit, errNumberConversion := strconv.Atoi(strDigit) //I think plain 'err' is too ambiguous. Plus it's already declared!
+		if errNumberConversion != nil {
+			return errNumberConversion, 0
+		}
 		//check if it's legit
-		if (((len(digits) - 1 - i) % 2) == 1) && (i != (len(digits) - 1)) {
-			number *= 2
-			if number > 9 {
-				number -= 9
+		shouldDouble := (((len(digits) - 1 - i) % 2) == 1) && (i != (len(digits) - 1))
+		if shouldDouble {
+			numDigit *= 2
+			if numDigit > 9 {
+				numDigit -= 9
 			}
 		}
-		//convert back to a string
-		//doubled = strconv.Itoa(number) + doubled
-		if numberCheck != nil {
-			return numberCheck, 0
-		}
-		summed += number
+		summed += numDigit
 	}
 	return nil, summed
 }
