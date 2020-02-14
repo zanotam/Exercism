1edb35d481d781fd0ca1cef9065ff8846cd5affb
Robert Baker <zanotam@gmail.com>
2020-02-14 12:59:22 -0700

    Makes Luhn more idiomatic.

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -10,33 +10,32 @@ import (
 
 //Valid Checks if a string passes the Luhn test
 func Valid(digits string) (isValid bool) {
-	strippedDigits := strings.ReplaceAll(digits, " ", "")
-	err, summedDigits := luhnDoubleAndSum(strippedDigits)
+	digits = strings.ReplaceAll(digits, " ", "")
+	sum, err := luhnDoubleAndSum(digits)
 	if err == nil {
-		isValid = (summedDigits%10) == 0 && (len(strippedDigits) > 1)
+		isValid = (sum%10) == 0 && (len(digits) > 1)
 		return isValid
 	}
 	return false
 }
 
-//switched to error first because I like that convention
-func luhnDoubleAndSum(digits string) (err error, summed int) {
-	//var doubled string;
-	shouldDouble := ((len(digits) % 2) == 1) //this value naturally alternates every iteration but also starts differently depending upon odd/even.
+func luhnDoubleAndSum(digits string) (sum int, err error) {
+	//shouldDOuble naturally alternates every iteration but also starts differently depending upon odd/even.
+	shouldDouble := len(digits)%2 == 1
 	for i, digit := range digits {
 		shouldDouble = !shouldDouble
-		//convert rune representation into the integer it represents
-		numDigit, errNumberConversion := strconv.Atoi(string(digit)) //I think plain 'err' is too ambiguous. Plus it's already declared!
-		if errNumberConversion != nil {
-			return errNumberConversion, 0
+		numDigit, err := strconv.Atoi(string(digit))
+		if err != nil {
+			return 0, err
 		}
-		if (shouldDouble && (i != (len(digits) - 1))) { //includes a rare case which only happens at most once on I think odd terms.
+		//includes a rare case which only happens at most once on I think odd terms.
+		if shouldDouble && (i != (len(digits) - 1)) {
 			numDigit *= 2
 			if numDigit > 9 {
 				numDigit -= 9
 			}
 		}
-		summed += numDigit
+		sum += numDigit
 	}
-	return nil, summed
+	return sum, nil
 }
